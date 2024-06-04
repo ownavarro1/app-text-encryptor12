7d11f2dab16224ec3ebc0ea04e9418e255cc7987
ownavarro1 <112905824+ownavarro1@users.noreply.github.com>
2024-06-04 17:02:36 -0500

    v1

---
+++ README.md
@@ -0,0 +1,4 @@
+Challenge ONE | Lógica de Programación - Encriptador de texto
+![image](https://github.com/ownavarro1/app-text-encryptor1/assets/112905824/b79d07c5-e10d-4b5f-982e-41978620b5a9)
+¡Bienvenido al repositorio del Encriptador de texto! Pasos principales:
+Marca este proyecto con una estrella ⭐
---
+++ archivos/script.js
@@ -0,0 +1,61 @@
+const textArea =  document.querySelector(".text-area");
+const mensaje =  document.querySelector(".mensaje");
+
+function validarTexto(){
+    let validador = textArea.value.match(/^[a-z]*$/);
+    if(!validador || validador === 0) {
+        alert("Solo son permitidas letras minúsculas y sin acentos")
+        location.reload();
+        return true;
+    }
+}
+function btnEncriptar(){
+    if(!validarTexto()) {
+    textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+    }        
+}
+
+function encriptar(stringEncriptado){
+let matrizCodigo = [["e","enter"],["i","ines"],["a","ia"],["o","ober"],["u","ufat"]];
+stringEncriptado = stringEncriptado.toLowerCase();
+
+for(let i=0;i<matrizCodigo.length;i++) {
+    if(stringEncriptado.includes(matrizCodigo[i][0])) {
+        stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+    }
+}
+
+return stringEncriptado
+}
+
+function btnCopiar() {
+   textoCopiado = mensaje.value
+   textArea.value = textoCopiado;
+   mensaje.value = "";
+    console.log(textoCopiado)
+}
+
+function btnDesencriptar(){
+    
+    textoEncriptado = desencriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    
+   
+}
+
+function desencriptar(stringDesencriptado){
+    let matrizCodigo = [["e","enter"],["i","ines"],["a","ia"],["o","ober"],["u","ufat"]];
+    console.log(matrizCodigo);
+    stringDesencriptado= stringDesencriptado.toLowerCase();
+    for(let i=0;i<matrizCodigo.length;i++) {
+        if(stringDesencriptado.includes(matrizCodigo[i][1])) {
+            stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0])
+        }
+    }
+    
+    return stringDesencriptado
+}
